01cc8fbcab89cc38ddce44a6d832efcb00a69074
Santos Alarcón <santosalarcon86@gmail.com>
2023-09-27 19:09:00 +0200

    Modified build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://santosalarcon.github.io/AstroTest",
+    build: {
+        assets: "_astro"
+    }
 });
